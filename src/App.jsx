65b3bd37a3640e4f2949e05bc56d0af5d6b304a1
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-05-03 10:04:48 +0400

    fixed deployment settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import StartHere from "./pages/StartHere";
 import Login from "./pages/Login";
-import TodoList from "./pages/todoList";
+import TodoList from "./pages/List";
 import { UserProvider } from "./context/UserProvider";
 
 function App() {
